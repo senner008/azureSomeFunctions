f677d4865859bbb545d20fa20844ecf741fc9076
nig <nig@beepanalytics.com>
2020-05-09 14:50:46 +0200

    changes test

--- HttpTrigger/index.ts
+++ HttpTrigger/index.ts
@@ -7,7 +7,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     if (name) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hi " + (req.query.name || req.body.name)
+            body: "Hi test " + (req.query.name || req.body.name)
         };
     }
     else {
